5e14c651570dab0d1e13ea289e0551c2272862cf
ploypertpinkz <za_magic_keen@hotmail.com>
2019-08-10 10:40:01 +0700

    homework: Add reset button

--- app/src/main/java/buu/informatics/s59160587/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160587/diceroller/MainActivity.kt
@@ -13,11 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton:Button = findViewById(R.id.roll_button)
+        val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = "Dice Rolled!"
+
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener { resetDice() }
 
     }
 
@@ -26,6 +29,11 @@ class MainActivity : AppCompatActivity() {
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+    }
 
+    private fun resetDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "0"
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 }
